eb55dcbdb3d6ded7d512c573b476cb0d0e124ccb
dmacedo <devinm3333@outlook.com>
2025-07-22 06:48:03 -0700

    fixed partials path for blogs page

--- models/post.js
+++ models/post.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  owner:    {type: String, required: true},
-  content:  {type: String, required: true},
-  id:       {type: Number, required: true},
-  created:  {type: Date, required: true},
-  deleted:  {type: Date, required: false},
+  title: String,
+  author: String,
+  content: String,
+  created: Date,
+  deleted: Date,
+  comments: [{
+    author: String,
+    content: String,
+    created: Date,
+    deleted: Date,
+  }],
 });
 
 const Post = mongoose.model('Post', postSchema);
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const localPort = 3000;
 const mongoose = require('mongoose');
-const Blog = require('./models/blog.js');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require("morgan");
+const path = require("path");
+const Post = require('./models/post.js');
 
-mongoose.connect(process.env.MONGO_URI);
+dotenv.config();
+const app = express();
+app.use(express.urlencoded({ extended: false}));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -12,5 +25,21 @@ app.get('/', (req,res) => {
 });
 
 app.get('/home', (req,res) => {
-  app.render('views/home.ejs');
+  res.render('home.ejs');
+});
+
+app.get('/blogs', (req,res) => {
+  res.render('blogs/blogs.ejs');
+});
+
+app.post('/blogs/new', (req,res) => {
+  res.render('blogs.ejs');
+});
+
+app.get('/blogs/:id',(req,res) => {
+  res.send(`This is the route for: ${req.params.id}`);
+});
+
+app.listen(localPort, () => {
+  console.log(`Listening on port ${localPort}`);
 });
